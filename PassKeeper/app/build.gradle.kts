61aa5213bc422011d869d4d32b887e2136892426
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-08 01:17:50 -0300

    Add Hash

--- PassKeeper/app/build.gradle.kts
+++ PassKeeper/app/build.gradle.kts
@@ -40,10 +40,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Para convertir JSON a objetos Java
     implementation("com.github.f0ris.sweetalert:library:1.5.6") //alertas Sweet Alert
-
-
-
+    implementation ("org.mindrot:jbcrypt:0.4") //para hashing 
 }
