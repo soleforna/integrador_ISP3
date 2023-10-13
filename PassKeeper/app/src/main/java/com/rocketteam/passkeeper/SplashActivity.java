937e7eb2f66beba909b0b0b95a1f62207fc44159
Marcos Barey <caposelmarco@gmail.com>
2023-10-13 15:08:42 -0300

    Update SplashActivity
    se agrego un titulo y un icono para funcionalidad

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/SplashActivity.java
+++ PassKeeper/app/src/main/java/com/rocketteam/passkeeper/SplashActivity.java
@@ -3,17 +3,21 @@ package com.rocketteam.passkeeper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt; //importación para la autentificación biometrica
 import androidx.core.content.ContextCompat;//importación para el contextCompat
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
+    private ImageView imageViewFinger; //se instancia el imagenViewFinger
     private BiometricPrompt biometricPrompt; //se instancia el BiometricPromt
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        imageViewFinger = findViewById(R.id.imageViewFinger);
         boolean isLoguedIn = true;
 
         if (isLoguedIn) {
@@ -25,6 +29,12 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(intent);
             finish(); // Cierra la actividad actual para que el usuario no pueda volver atrás
         }
+        imageViewFinger.setOnClickListener(new View.OnClickListener() { //onclik para mostrar el DialogoDeAutenticacion
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoDeAutenticacion();
+            }
+        });
 
     }
 
@@ -35,13 +45,15 @@ public class SplashActivity extends AppCompatActivity {
             public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
                 // La autenticación fue exitosa
                 startActivity(new Intent(SplashActivity.this, PasswordsActivity.class));
+                Toast.makeText(SplashActivity.this, "Aunticado con exito", Toast.LENGTH_SHORT).show();
+                finish();
             }
 
             @Override
             public void onAuthenticationError(int errorCode, CharSequence errString) {
                 if (errorCode == BiometricPrompt.ERROR_NO_BIOMETRICS) {
                     // En el caso de que no haya huellas dactilares registradas o la autenticación biométrica no es posible
-                    // Redirige al usuario a una pantalla de inicio de sesión
+                    //Todo hacer algo en este caso
                 } else {
                     // Otro error de autenticación biométrica
                     Toast.makeText(SplashActivity.this, errString, Toast.LENGTH_SHORT).show();
@@ -51,6 +63,7 @@ public class SplashActivity extends AppCompatActivity {
                 public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
                     // Se requiere ayuda para autenticar
                     Toast.makeText(MainActivity.this, helpString, Toast.LENGTH_SHORT).show();
+                    //en el caso de que se desee agregar alguna opción de ayuda al usuario
                 }*/
         });
 
@@ -58,4 +71,5 @@ public class SplashActivity extends AppCompatActivity {
 
         biometricPrompt.authenticate(promptInfo);
     }
+
 }
