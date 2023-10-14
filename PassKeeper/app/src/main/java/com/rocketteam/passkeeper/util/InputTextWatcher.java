73ac9d09f15156d130c2782051c747bc134e173f
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-14 00:05:14 -0300

    Update InputTextWatcher.java

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/util/InputTextWatcher.java
+++ PassKeeper/app/src/main/java/com/rocketteam/passkeeper/util/InputTextWatcher.java
@@ -4,14 +4,31 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import com.google.android.material.textfield.TextInputLayout;
 
+/**
+ * Clase personalizada que implementa TextWatcher para validar campos de texto en tiempo real.
+ * Permite mostrar un mensaje de error en un TextInputLayout si el campo está vacío.
+ */
 public class InputTextWatcher implements TextWatcher {
 
     private TextInputLayout textInputLayout;
 
+    /**
+     * Constructor que toma un TextInputLayout como parámetro.
+     *
+     * @param textInputLayout TextInputLayout asociado al campo de texto que se debe validar.
+     */
     public InputTextWatcher(TextInputLayout textInputLayout) {
         this.textInputLayout = textInputLayout;
     }
 
+    /**
+     * Se llama cuando el texto en el campo de texto cambia.
+     *
+     * @param s      Texto actual en el campo de texto.
+     * @param start  Índice del primer carácter modificado.
+     * @param before Número de caracteres eliminados.
+     * @param count  Número de caracteres agregados.
+     */
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         // Realiza acciones según el texto cambiado
@@ -23,10 +40,23 @@ public class InputTextWatcher implements TextWatcher {
         }
     }
 
+    /**
+     * Se llama antes de que el texto en el campo de texto cambie.
+     *
+     * @param s      Texto actual en el campo de texto.
+     * @param start  Índice del primer carácter a ser cambiado.
+     * @param count  Número de caracteres a ser cambiados.
+     * @param after  Número de caracteres que serán agregados después de 'start'.
+     */
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
     }
 
+    /**
+     * Se llama después de que el texto en el campo de texto cambia.
+     *
+     * @param s Texto actual en el campo de texto después del cambio.
+     */
     @Override
     public void afterTextChanged(Editable s) {
     }
