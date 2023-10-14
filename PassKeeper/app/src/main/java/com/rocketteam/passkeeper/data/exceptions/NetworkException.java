1eea25d1bd4e6f35f79f2f583239482865724fc9
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-14 00:09:46 -0300

    Delete NetworkException.java

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/data/exceptions/NetworkException.java
+++
@@ -1,9 +0,0 @@
-package com.rocketteam.passkeeper.data.exceptions;
-
-import java.io.IOException;
-
-public class NetworkException extends IOException {
-    public NetworkException(String message) {
-        super(message);
-    }
-}
