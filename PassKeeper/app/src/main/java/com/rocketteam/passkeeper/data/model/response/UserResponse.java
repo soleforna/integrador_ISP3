d012f207fb32f660be945b7453794de77f7f8c88
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-14 13:26:28 -0300

    Update UserResponse.java

---
+++ PassKeeper/app/src/main/java/com/rocketteam/passkeeper/data/model/response/UserResponse.java
@@ -0,0 +1,47 @@
+package com.rocketteam.passkeeper.data.model.response;
+
+public class UserResponse {
+    private int id;
+    private String email;
+    private String password;
+    private String salt;
+
+    public UserResponse(int id, String email, String password, String salt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.salt = salt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
