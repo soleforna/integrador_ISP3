0385c9832e8d4234641bed28f8bd927114ad3936
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-14 00:09:43 -0300

    Delete AuthenticationException.java

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/data/exceptions/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.rocketteam.passkeeper.data.exceptions;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
