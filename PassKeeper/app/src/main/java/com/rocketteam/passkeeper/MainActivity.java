2f48b3de10c9415cef5a3246013c0de7540589c6
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-10-21 01:08:32 -0300

    Update MainActivity.java

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/MainActivity.java
+++ PassKeeper/app/src/main/java/com/rocketteam/passkeeper/MainActivity.java
@@ -1,6 +1,11 @@
     package com.rocketteam.passkeeper;
     
+    import androidx.annotation.NonNull;
     import androidx.appcompat.app.AppCompatActivity;
+    import androidx.biometric.BiometricPrompt;
+
+    import android.content.Context;
+    import android.content.SharedPreferences;
     import android.os.Bundle;
     import android.content.Intent;
     import android.util.Log;
@@ -9,9 +14,11 @@
     import com.google.android.material.textfield.TextInputEditText;
     import com.google.android.material.textfield.TextInputLayout;
     import com.rocketteam.passkeeper.data.db.DbManager;
+    import com.rocketteam.passkeeper.util.BiometricUtils;
     import com.rocketteam.passkeeper.util.HashUtility;
     import com.rocketteam.passkeeper.util.InputTextWatcher;
     import android.text.TextUtils;
+    import android.widget.Toast;
 
     import java.security.NoSuchAlgorithmException;
     import java.util.Objects;
@@ -24,13 +31,14 @@
         private TextInputEditText editTextPassword;
         private TextInputLayout textInputLayoutEmail;
         private TextInputLayout textInputLayoutPwd;
-    
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             Log.i("TAG", "PasKeeper Iniciado");
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-    
+
+            SharedPreferences sharedPreferences = getSharedPreferences("Storage", Context.MODE_PRIVATE);
             dbManager = new DbManager(getApplicationContext());
             editTextEmail = findViewById(R.id.editTextUsername);
             editTextPassword = findViewById(R.id.editPassword);
@@ -41,6 +49,25 @@
             editTextEmail.addTextChangedListener(new InputTextWatcher(textInputLayoutEmail));
             editTextPassword.addTextChangedListener(new InputTextWatcher(textInputLayoutPwd));
 
+            int bio = sharedPreferences.getInt("biometric",-1);
+            Log.i("TAG", "Login Biometric: "+bio);
+            if(bio == 1) {
+                // Si el usuario ha configurado la preferencia para usar la autenticación biométrica
+                // Mostrar el cuadro de diálogo de autenticación biométrica
+                if (BiometricUtils.isBiometricPromptEnabled(MainActivity.this)) {
+                    // Si el dispositivo es compatible con la autenticación biométrica
+                    BiometricUtils.showBiometricPrompt(MainActivity.this, new BiometricPrompt.AuthenticationCallback() {
+                        @Override
+                        public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                            super.onAuthenticationSucceeded(result);
+                            // La autenticación biométrica fue exitosa
+                            startActivity(new Intent(MainActivity.this, PasswordsActivity.class));
+                            Toast.makeText(MainActivity.this, "Aunticado con exito", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                }
+            }
 
             /*
               Busca el botón de inicio de sesión en la interfaz de usuario y agrega un escuchador
@@ -48,8 +75,7 @@
              */
             Button btnLogin = findViewById(R.id.btn_login_m);
             btnLogin.setOnClickListener(v -> {
-                // Verifica si la entrada del usuario es válida antes de intentar iniciar sesión.
-                if (validateInput()) {
+                if (validateInput()) {// Verifica si la entrada del usuario es válida antes de intentar iniciar sesión.
                     try {
                         // Intenta iniciar sesión, manejando posibles excepciones de Hashing y NoSuchAlgorithmException.
                         iniciarSesion();
@@ -115,7 +141,7 @@
                 dbManager.close();
             } else {
                 // Si las credenciales son inválidas, muestra un mensaje de error.
-                mostrarSweetAlert(SweetAlertDialog.ERROR_TYPE, "Credenciales inválidas", "usuario o contraseña incorrecto");
+                mostrarSweetAlert();
                 // También podría usar Toast para mostrar un mensaje de error alternativo.
                 //Toast.makeText(this, "Credenciales inválidas, por favor intenta nuevamente", Toast.LENGTH_SHORT).show();
                 // Cierra la conexión con la base de datos.
@@ -124,17 +150,14 @@
 
         }
 
-
-
-        private void mostrarSweetAlert(int tipo, String titulo, String mensaje) {
-
+        private void mostrarSweetAlert() {
 
             // Crea una instancia de SweetAlertDialog con el contexto actual y el tipo especificado.
-            SweetAlertDialog sweetAlertDialog = new SweetAlertDialog(this, tipo);
+            SweetAlertDialog sweetAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE);
             // Establece el título del diálogo.
-            sweetAlertDialog.setTitleText(titulo);
+            sweetAlertDialog.setTitleText("Credenciales inválidas");
             // Establece el mensaje del diálogo.
-            sweetAlertDialog.setContentText(mensaje);
+            sweetAlertDialog.setContentText("usuario o contraseña incorrecto");
             // Establece el texto del botón de confirmación como "Aceptar".
             sweetAlertDialog.setConfirmText("Aceptar");
             // Establece un escuchador para el botón de confirmación.
@@ -143,7 +166,7 @@
                 sweetAlertDialog1.dismissWithAnimation();
 
                 // Realiza acciones adicionales en función del tipo de diálogo.
-                if (tipo == 2) {
+                if (SweetAlertDialog.ERROR_TYPE == 2) {
                     finish(); // Cerrar la actividad en caso de un error de registro
                 }
             });
