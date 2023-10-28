6728cde25d097c98314dd86803beebc98a193736
Pala797 <106939529+Pala797@users.noreply.github.com>
2023-10-28 20:06:15 -0300

    agrege alert
    modificaciones para agregar alerta a borrar

--- PassKeeper/app/src/main/java/com/rocketteam/passkeeper/PasswordsActivity.java
+++ PassKeeper/app/src/main/java/com/rocketteam/passkeeper/PasswordsActivity.java
@@ -1,6 +1,9 @@
 package com.rocketteam.passkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import android.content.DialogInterface;
+import android.widget.Button;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -44,7 +47,7 @@ public class PasswordsActivity extends AppCompatActivity {
     private ImageButton iconPen;
     private ImageButton iconTrash;
     private  List<PasswordResponse> passwords;
-
+    private int userId;
 
 
     @Override
@@ -75,7 +78,8 @@ public class PasswordsActivity extends AppCompatActivity {
                 Log.i("TAG", "La lista llega VACIA");
             }
 
-            MostrarPasswords(passwords);
+            MostrarPasswords(passwords, userId);
+
         } else {
             // La clave "userId" no existe
             Log.e("PasswordsActivity", "La clave 'userId' no existe");
@@ -250,19 +254,18 @@ public class PasswordsActivity extends AppCompatActivity {
 
 
 
-    private void MostrarPasswords(List<PasswordResponse> passwords) {
+    private void MostrarPasswords(List<PasswordResponse> passwords, int userId) {
         try {
-
-            TableLayout tableLayout = findViewById(R.id.tableLayout);//Obtenemos el tableLayout
-            TextView noPasswordsText = findViewById(R.id.txtNoPassword);//Obtenemos el textView
-            ImageView circleExclamation = findViewById(R.id.imageView);//Obtenemos el imageView
+            TableLayout tableLayout = findViewById(R.id.tableLayout);
+            TextView noPasswordsText = findViewById(R.id.txtNoPassword);
+            ImageView circleExclamation = findViewById(R.id.imageView);
             tableLayout.removeAllViews();
-            //si la lista esta vacia muestra cartel de advertencia
+
             if (passwords.isEmpty()) {
-                noPasswordsText.setVisibility(View.VISIBLE); //NO hay contraseñas
-                circleExclamation.setVisibility(View.VISIBLE); //Signo de Admiracion
-                tableLayout.setVisibility(View.GONE);//oculta la tabla
-            } else { //sino completa la tabla de password
+                noPasswordsText.setVisibility(View.VISIBLE);
+                circleExclamation.setVisibility(View.VISIBLE);
+                tableLayout.setVisibility(View.GONE);
+            } else {
                 noPasswordsText.setVisibility(View.GONE);
                 circleExclamation.setVisibility(View.GONE);
                 tableLayout.setVisibility(View.VISIBLE);
@@ -271,8 +274,6 @@ public class PasswordsActivity extends AppCompatActivity {
 
                 for (int i = 0; i < passwords.size(); i++) {
                     PasswordResponse pwd = passwords.get(i);
-
-                    // Esto crea un nuevo tableRow para cada contraseña,el tableRow esta en row_password.xml
                     TableRow row = (TableRow) inflater.inflate(R.layout.row_password, null);
 
                     iconEye = row.findViewById(R.id.icon_eye);
@@ -280,33 +281,31 @@ public class PasswordsActivity extends AppCompatActivity {
                     iconTrash = row.findViewById(R.id.icon_trash);
 
                     TextView nombreTextView = row.findViewById(R.id.textView);
-                    nombreTextView.setText(pwd.getName()); //Setea el PASSWORD_NAME AL textView
+                    nombreTextView.setText(pwd.getName());
+                    asignarBotones(pwd.getId(), userId);
 
-                    asignarBotones(pwd.getId());
-                    // Agrega el TableRow al TableLayout
                     tableLayout.addView(row);
                 }
-
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }catch (Exception e){
-            Log.e("TAG", "ERROR: "+e.getMessage());
+        } catch (Exception e) {
+            Log.e("TAG", "ERROR: " + e.getMessage());
         }
     }
+
 
     protected void onDestroy() {
         super.onDestroy();
         dbManager.close(); // Cierra la base de datos al destruir la actividad.
     }
 
-    private void asignarBotones(int id) {
+    private void asignarBotones(int id, int userId) {
         iconTrash.setTag(id); // Donde 'id' es el ID de la contraseña correspondiente
 
         iconEye.setOnClickListener(new View.OnClickListener() {
 
-            //onclick para abrir la actividad del ViewPassActivity
+            // onclick para abrir la actividad del ViewPassActivity
             @Override
             public void onClick(View v) {
 
@@ -314,13 +313,14 @@ public class PasswordsActivity extends AppCompatActivity {
                 Intent intent = new Intent(PasswordsActivity.this, ViewPassActivity.class);
                 // Agrega el id como un extra en el intent
                 intent.putExtra("idColumna", id);
-                // utlizamos el putExtra para pasar información con el intent
+                // Agrega userId como un extra en el intent
+                intent.putExtra("userId", userId);
                 // Inicia la actividad ViewPassActivity
                 startActivity(intent);
             }
         });
 
-        // onclick del el editar
+        // onclick del editar
         iconPen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -328,40 +328,50 @@ public class PasswordsActivity extends AppCompatActivity {
                 Intent intent = new Intent(PasswordsActivity.this, EditarPassword.class);
                 // Agrega el id como un extra en el intent
                 intent.putExtra("idColumna", id);
-                // utlizamos el putExtra para pasar información con el intent
+                // Agrega userId como un extra en el intent
+                intent.putExtra("userId", userId);
                 // Inicia la actividad ViewPassActivity
                 startActivity(intent);
 
             }
         });
+
         // Onclick Borrar
-         iconTrash.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                // Obtén el ID de la contraseña a eliminar desde la vista
-                int idToDelete = (int) v.getTag(); //  tag de la vista con el ID
+        iconTrash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int idToDelete = (int) v.getTag();
 
-                // Llama al método eliminarContraseña con el ID
-                dbManager.deletePassword(idToDelete);
-                // Actualiza la lista de contraseñas eliminando el elemento correspondiente
-                PasswordResponse deletedPassword = null;
-                for (PasswordResponse password : passwords) {
-                    if (password.getId() == idToDelete) {
-                        deletedPassword = password;
-                        break;
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PasswordsActivity.this);
+                alertDialogBuilder.setTitle("Confirmar Eliminación");
+                alertDialogBuilder.setMessage("¿Estás seguro de que deseas eliminar esta contraseña?");
+
+                alertDialogBuilder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
                     }
-                }
-                if (deletedPassword != null) {
-                    passwords.remove(deletedPassword);
-                }
+                });
 
-                // Actualiza la vista para reflejar los cambios después de la eliminación
-                MostrarPasswords(passwords);
-             }
-        } );
-     }
+                alertDialogBuilder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dbManager.deletePassword(idToDelete);
 
-        public List<PasswordResponse> filterPasswords(List<PasswordResponse> passwords) {
+                        // Actualiza la vista después de eliminar la contraseña
+                        passwords = dbManager.getPasswordsListForUserId(userId);
+                        MostrarPasswords(passwords, userId);
+
+
+                        dialog.dismiss();
+                    }
+                });
+
+                alertDialogBuilder.create().show();
+            }
+        });
+    }
+
+
+    public List<PasswordResponse> filterPasswords(List<PasswordResponse> passwords) {
         List<PasswordResponse> filterpass= new ArrayList<>();
 
         return filterpass;
